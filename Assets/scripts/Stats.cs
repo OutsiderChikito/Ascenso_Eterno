03de4cbe26a1e6e626ba0a68819dd8f8aa89ae2d
MiguelNavarro <nbmiguelandres@gmail.com>
2025-10-28 21:49:33 -0500

    cambios avance 2

--- Assets/scripts/Stats.cs
+++ Assets/scripts/Stats.cs
@@ -8,10 +8,25 @@ public class Stats : MonoBehaviour
     public int monedas = 0;
     [Header("TextosUI")]
     public TextMeshProUGUI monedastxt;
+    [Header("Configuración")]
+    public GameObject objetoPortal; // El objeto 3D que se activará
+    public int monedasPorRecolectar = 10;
+
+    void Start()
+    {
+        // Aseguramos que el objeto comience desactivado
+        if (objetoPortal != null)
+            objetoPortal.SetActive(false);
+    }
 
     private void Update()
     {
         monedastxt.text = "MONEDAS: " + monedas.ToString();
+
+        if (objetoPortal != null && monedas >= monedasPorRecolectar)
+        {
+            objetoPortal.SetActive(true);
+        }
     }
     
 }
