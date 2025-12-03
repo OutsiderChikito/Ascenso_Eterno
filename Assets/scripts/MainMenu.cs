3e34b6e7f2e73b04b9f93c3d48708172d2264ea7
XtreamX2002 <ymanolhg@gmail.com>
2025-12-03 01:24:25 -0500

    MainMenu(Yh)

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Nivel_1");
+    }
+
+}
